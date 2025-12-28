bf1df58d4dec488dda8b2a76dc465818e8173343
ebuleje-dev <edgard.bulejev@hotmail.com>
2025-12-28 00:34:09 -0500

    add docker file and connect git config

--- src/main/java/com/nttd/banking/config/ConfigServerApplication.java
+++ src/main/java/com/nttd/banking/config/ConfigServerApplication.java
@@ -2,10 +2,17 @@ package com.nttd.banking.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
+    /**
+     * Main method to start the Config Server application.
+     *
+     * @param args command line arguments
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
